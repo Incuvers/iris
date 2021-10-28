b0d7e88936bbd1afbd34c180a2c040bfd912052f
cSDes1gn <christian911@sympatico.ca>
2021-10-28 11:53:14 -0400

    stripping all hwi interfaces

--- monitor/flash_service/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#  Copyright (C) Incuvers, Inc - All Rights Reserved
-#  Unauthorized copying of this file, via any medium is strictly prohibited
-#  Proprietary and confidential
-#  Written by Travis Haycock
-#
