4c71de0e8eb9636a87c9e070c7b04bcf5df98038
cSDes1gn <christian911@sympatico.ca>
2021-11-03 17:21:18 -0400

    script for generating rmq password hashes

---
+++ scripts/rmq-pass-gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -sp 'Enter user password: ' password
+
+function encode_password()
+{
+    SALT=$(od -A n -t x -N 4 /dev/urandom)
+    PASS=$SALT$(echo -n $1 | xxd -ps | tr -d '\n' | tr -d ' ')
+    PASS=$(echo -n $PASS | xxd -r -p | sha256sum | head -c 128)
+    PASS=$(echo -n $SALT$PASS | xxd -r -p | base64 | tr -d '\n')
+    printf "%b" "\n$PASS"
+}
+
+encode_password "$password"
