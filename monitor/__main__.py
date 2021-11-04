83b021c808aa75b7d47f6e5cdd3c4bb4dbb4e730
cSDes1gn <christian911@sympatico.ca>
2021-11-04 11:48:12 -0400

    remove headless mode in main entry

--- monitor/__main__.py
+++ monitor/__main__.py
@@ -13,47 +13,41 @@ from monitor.microscope.microscope import Microscope
 from monitor.events.event_handler import EventHandler
 from monitor.sys.system import service_boot, system_boot
 from monitor.environment.thread_manager import ThreadManager
-from monitor.environment.context_manager import ContextManager
 from monitor.ui.user_interface_controller import UserInterfaceController
 ```
 Copyright © 2021 Incuvers. All rights reserved.
 Unauthorized copying of this file, via any medium is strictly prohibited
 Proprietary and confidential
 """
+import os
 import logging
 from monitor.api.proxy import ApiProxy
 from monitor.sys.system import service_boot, main
 from monitor.environment.thread_manager import ThreadManager
-# from monitor.microscope.microscope import Microscope as scope
-from monitor.environment.context_manager import ContextManager
 from monitor.ui.main import UserInterfaceController
 
 # Initiate Logging
 _logger = logging.getLogger(__name__)
 
 # setup thread watch
-thread_monitor = ThreadManager().start()
-# microscope init
-# scope.init()
-with ContextManager() as context:
-    mode = context.get_env('MONITOR_MODE')
+ThreadManager().start()
 
-if mode == 'headless':
-    _logger.debug("Entering Headless Mode")
-    ApiProxy()
-    main()
-    thread_monitor.join()
-elif mode == 'service':
+# get environment variables
+mode = os.environ.get('MONITOR_MODE', default="monitor")
+_logger.info("Monitor mode: %s", mode)
+base_url = os.environ.get('API_BASE_URL', default="https://api.prod.incuvers.com")
+base_path = os.environ.get('API_BASE_PATH', default="/v1")
+
+# start ui
+uic = UserInterfaceController(mode)
+
+if mode == 'service':
     _logger.info("Entering Service Mode")
-    # start ui
-    uic = UserInterfaceController()
     service_boot()
     # load runtime models from cache into state manager
     uic.service_loop()
 elif mode == 'monitor':
     _logger.info("Entering Monitor Mode")
-    # start ui
-    uic = UserInterfaceController()
-    ApiProxy()
+    ApiProxy(base_url, base_path)
     main()
     uic.ui_loop()
