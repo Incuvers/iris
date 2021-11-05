3a9795c4b321547c50da298cd977538865aa8a20
cSDes1gn <christian911@sympatico.ca>
2021-11-05 16:34:18 -0400

    clean init

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -9,15 +9,9 @@ Unauthorized copying of this file, via any medium is strictly prohibited
 Proprietary and confidential
 """
 
-import os
-import sys
 import logging
 import logging.config
-from pathlib import Path
-from envyaml import EnvYAML
-from configparser import ConfigParser
 
-from monitor.logs.formatter import pformat
 from monitor.__version__ import __version__
 
 logging.basicConfig(
@@ -25,72 +19,3 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s"
 )
 logging.info("Incuvers™ Monitor Version: %s", __version__)
-
-
-def logging_handler(config_path: Path, base_path: str) -> None:
-    """
-    Configure monitor logger using dict config and set the logging path
-
-    :param config_path: path to log config
-    :type config_path: Path
-    :param base_path: logging path
-    :type base_path: str
-    """
-    os.makedirs(base_path, mode=0o777, exist_ok=True)
-    # using split '.' to remove logs for rolling file handlers with format: <name>.log.<number>
-    logs = list(
-        filter(
-            lambda file: 'log' in file.split('.'),
-            os.listdir(path=base_path)
-        )
-    )
-    # purge old logs on new instance
-    for log in logs: os.remove(base_path + '/' + log)
-    # bind logging to config file
-    # verify path existance before initializing logger file configuration
-    try:
-        # load config from .yaml
-        env = EnvYAML(config_path).export()
-        logging.info("Parsed logger config:%s", pformat(env))
-        logging.config.dictConfig(env)
-        logging.info('Configuring logger using dict config')
-    except ValueError as exc:
-        logging.exception(
-            "Logging configuration failed due to missing environment variables: %s", exc)
-    except FileNotFoundError:
-        logging.exception(
-            "Logging config file not found in expected absolute path: {}".format(config_path))
-    else:
-        logging.info("Logging configuration successful.")
-
-def device_certs_handler(base_path: str) -> None:
-    """
-    Read device certs and export as environment variables for global access. 
-    If device certs are missing exit with error code 2
-
-    :param base_path: device certs base path
-    :type base_path: str
-    """
-    if not os.path.exists(base_path + '/amqp.ini') or \
-            not os.path.exists(base_path + '/device.ini'):
-        logging.critical("Failed to identify device certs.")
-        sys.exit(2)
-    # instantiate
-    config = ConfigParser()
-    config.read(base_path + '/amqp.ini')
-    os.environ['AMQP_USER'] = config.get('amqp', 'user')
-    os.environ['AMQP_PASS'] = config.get('amqp', 'password')
-    # parse existing file
-    config.read(base_path + '/device.ini')
-    os.environ['ID'] = config.get('iris', 'id')
-    logging.info("Successfully exported device certs")
-
-logging_handler(
-    config_path=Path(__file__).parent.joinpath("logs/config/config.yml"),
-    base_path=os.environ.get("MONITOR_LOGS", str(Path(__file__).parent.joinpath('logs/')))
-)
-
-device_certs_handler(
-    base_path=os.environ.get("MONITOR_CERTS", str(
-        Path(__file__).parent.parent.joinpath('instance/certs')))
-)
