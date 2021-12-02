8c97aadbb54e11764414a722484be2645ef9b8a2
cSDes1gn <christian@incuvers.com>
2021-12-02 21:59:56 +0000

    add panels (widget + offsets) for clarity

--- monitor/ui/views/canvas.py
+++ monitor/ui/views/canvas.py
@@ -17,30 +17,38 @@ Unauthorized copying of this file, via any medium is strictly prohibited
 Proprietary and confidential
 """
 
-class Canvas:
+from typing import List
+from monitor.ui.components.widget import Widget
 
-    def __init__(self, surface_height, surface_width):
-        """
-        init
-        """
-        self.width = surface_width
-        self.height = surface_height
-        self.widgets = []
-        self.coords = []
 
-    def add_info_widget(self, widget, y_offset):
-        self.widgets.append(widget)
-        self.coords.append((0, y_offset))
-
-    def add_gauge_widget(self, widget, loc_idx):
-        # x,y grid index based on loc_idx = 0,1,2,3
-        y_offset = self.height - widget.height*2
-        i = loc_idx % 2
-        j = loc_idx // 2
-        self.widgets.append(widget)
-        self.coords.append((widget.width*i, y_offset+widget.height*j))
+class Panel:
+    def __init__(self, widget: Widget, x_offset: int, y_offset: int) -> None:
+        self.widget = widget
+        self.x_offset = x_offset
+        self.y_offset = y_offset
 
     def redraw(self, main_surface):
-        for idx in range(len(self.widgets)):
-            self.widgets[idx].redraw()
-            main_surface.blit(self.widgets[idx].get_surface(), self.coords[idx])
+        self.widget.redraw()
+        main_surface.blit(self.widget.get_surface(), (self.x_offset, self.y_offset))
+
+
+class Canvas:
+
+    def __init__(self, surface_height: int, surface_width: int) -> None:
+        self.width = surface_width
+        self.height = surface_height
+        self.panels: List[Panel] = []
+
+    def add_panel(self, widget: Widget, x_offset: int, y_offset: int):
+        self.panels.append(Panel(widget, x_offset, y_offset))
+
+    # def add_gauge_widget(self, widget, loc_idx):
+    #     # x,y grid index based on loc_idx = 0,1,2,3
+    #     y_offset = self.height - widget.height * 2
+    #     i = loc_idx % 2
+    #     j = loc_idx // 2
+    #     self.widgets.append(widget)
+    #     self.coords.append((widget.width * i, y_offset + widget.height * j))
+
+    def redraw(self, main_surface):
+        for idx in range(len(self.panels)): self.panels[idx].redraw(main_surface)
