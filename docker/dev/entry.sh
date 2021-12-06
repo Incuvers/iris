79073823d283b9bdef8633e9838b7fd8382d8451
cSDes1gn <christian@incuvers.com>
2021-12-06 21:23:16 +0000

    string parse host and port from addr

--- docker/dev/entry.sh
+++ docker/dev/entry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+HOST="$(cut -d':' -f1<<<"$RABBITMQ_ADDR")"
+PORT="$(cut -d':' -f2<<<"$RABBITMQ_ADDR")"
 
 # wait for rabbitmq container
-# ./docker/dev/wfi.sh -h rmq -p 5672
+./docker/dev/wfi.sh -h "$HOST" -p "$PORT" -t 30
 
 python3 -m monitor
