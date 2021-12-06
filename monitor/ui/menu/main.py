5c3768238e618e18b2feb291fd7b3aa2093a41d9
cSDes1gn <christian@incuvers.com>
2021-12-06 21:26:00 +0000

    docstring

--- monitor/ui/menu/main.py
+++ monitor/ui/menu/main.py
@@ -7,15 +7,9 @@ Modified: 2021-07
 Dependencies:
 -------------
 ```
-import time
+import pygame
 
-from monitor.sys import system
-from monitor.models.experiment import Experiment
-from monitor.events.registry import Registry as events
-from monitor.environment.state_manager import StateManager
-from monitor.environment.thread_manager import ThreadManager as tm
-from monitor.microscope.microscope import Microscope as scope
-from monitor.microscope.fluorescence.hardware import FluorescenceHardware
+from pygame import USEREVENT  # type: ignore
 from monitor.ui.menu.info import InfoMenu
 from monitor.ui.menu.pgm.menu import Menu
 from monitor.ui.menu.pgm import events as pge
@@ -24,10 +18,6 @@ from monitor.ui.menu.sensors.co2 import CO2Menu
 from monitor.ui.menu.sensors.fan import FanMenu
 from monitor.ui.menu.sensors.temp import TempMenu
 from monitor.ui.static.settings import UISettings as uis
-from monitor.ui.menu.confirmation import ConfirmationMenu
-from monitor.ui.menu.stream.focus import FocusMenu
-from monitor.ui.menu.stream.exposure.phase import PhaseExposureMenu
-from monitor.ui.menu.stream.exposure.gfp import GFPExposureMenu
 ```
 Copyright © 2021 Incuvers. All rights reserved.
 Unauthorized copying of this file, via any medium is strictly prohibited
