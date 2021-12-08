41c48686e6a52c0f52c24678186fd0815d9ac353
cSDes1gn <christian911@sympatico.ca>
2021-12-08 17:41:44 -0500

    remove entry and wfi (add wfi python handler inside monitor)

--- docker/dev/entry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HOST="$(cut -d':' -f1<<<"$RABBITMQ_ADDR")"
-PORT="$(cut -d':' -f2<<<"$RABBITMQ_ADDR")"
-
-# wait for rabbitmq container
-./docker/dev/wfi.sh -h "$HOST" -p "$PORT" -t 30
-
-python3 -m monitor
